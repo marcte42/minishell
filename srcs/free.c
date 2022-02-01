6976f6189272cc5a3713ff80cc5bcc835a36f8d1
marcte42 <marcte42@gmail.com>
2022-02-01 20:49:44 +0100

    Je ne sais pas ce que je fais

--- srcs/free.c
+++ srcs/free.c
@@ -6,29 +6,11 @@
 /*   By: mterkhoy <mterkhoy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 16:36:47 by me                #+#    #+#             */
-/*   Updated: 2022/02/01 19:46:01 by mterkhoy         ###   ########.fr       */
+/*   Updated: 2022/02/01 15:33:02 by mterkhoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void ft_unlink(void *file)
-{
-	t_rdr	*rdr;
-
-	rdr = file;
-	if (rdr->type == 2)
-		unlink(rdr->file);
-}
-
-void	ft_lstcmd(t_list *lst,  void (*cmd)(void *))
-{
-	while (lst)
-	{
-		(*cmd)(lst->content);
-		lst = lst->next;
-	}
-}
 
 void	free_sys(t_sys *mini)
 {
@@ -40,7 +22,6 @@ void	free_sys(t_sys *mini)
 	while (tmp)
 	{
 		cmd = tmp->content;
-		ft_lstcmd(cmd->r_in, ft_unlink);
 		if (cmd->raw)
 			free(cmd->raw);
 		i = -1;
@@ -48,7 +29,6 @@ void	free_sys(t_sys *mini)
 			free(cmd->argv[i]);
 		free(cmd->argv);
 		free(cmd->clean);
-		
 		ft_lstclear(&cmd->r_in, free);
 		ft_lstclear(&cmd->r_out, free);
 		tmp = tmp->next;
