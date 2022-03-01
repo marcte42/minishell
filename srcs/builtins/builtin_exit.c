7621508f2ae329eeb0488f22e0d16bd9bf34db93
marcte42 <marcte42@gmail.com>
2022-03-01 17:26:57 +0100

    exit remaster

--- srcs/builtins/builtin_exit.c
+++ srcs/builtins/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: mterkhoy <mterkhoy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 13:19:25 by pravry            #+#    #+#             */
-/*   Updated: 2022/02/06 22:01:58 by mterkhoy         ###   ########.fr       */
+/*   Updated: 2022/03/01 17:25:44 by mterkhoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,8 @@ int	ft_full_strisnum(const char *str)
 
 int	ft_exit(char **args, t_sys *mini)
 {
-	if (args[1] && args[2])
-	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-		return (1);
-	}
 	if (mini->cmds_count == 1)
-		mini->exit = 1;
+			mini->exit = 1;
 	if (args[1] && ft_full_strisnum(args[1]) == 0)
 	{
 		ft_putstr_fd("exit\n", STDERR_FILENO);
@@ -46,6 +41,12 @@ int	ft_exit(char **args, t_sys *mini)
 		ft_putstr_fd(args[1], STDERR_FILENO);
 		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
 		return (2);
+	}
+	if (args[1] && args[2])
+	{
+		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+		mini->exit = 0;
+		return (1);
 	}
 	if (!args[1])
 		return (0);
